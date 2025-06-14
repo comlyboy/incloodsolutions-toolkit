935ca9a4e12159fc8dc79c2a5ef928f63537fc88
Cornelius Okeke <okekecornelius@gmail.com>
2025-06-14 23:56:39 +0100

    improve code, added mjs built

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
 	entry: ['src/**/*.ts'],
+	target: ['es2024'],
 	platform: 'node',
 	splitting: false,
 	sourcemap: true,
 	clean: true,
-	format: 'esm',
+	format: ['esm', 'cjs'],
 	bundle: false,
 	tsconfig: './tsconfig.json'
 });
