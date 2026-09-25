8ab533e14bb5c83577d76bc6c5a5d24077ee83bb
Cornelius Okeke <okekecornelius@gmail.com>
2026-09-25 23:14:10 +0100

    refactor: update tsup.config.ts to use base configuration and remove commented code

--- react/tsup.config.ts
+++ react/tsup.config.ts
@@ -1,24 +1,36 @@
-import { defineConfig } from "tsup";
+// import { defineConfig } from "tsup";
+
+// export default defineConfig([
+// 	{
+// 		entry: ['src/index.ts'],
+// 		format: ['esm'],
+// 		target: 'es2022',
+// 		// Bundle the whole public type surface into a single `dist/index.d.ts`.
+// 		dts: true,
+// 		sourcemap: true,
+// 		clean: true,
+// 		bundle: true,
+// 		splitting: false,
+// 		treeshake: true,
+// 		minify: false,
+// 		platform: 'browser',
+// 		keepNames: true,
+// 		external: ["tslib", "react", "react-dom", "react-router-dom"],
+// 		esbuildOptions(options) {
+// 			options.keepNames = true;
+// 			options.logLevel = 'error';
+// 		}
+// 	}
+// ]);
+
+import { defineConfig, Options } from "tsup";
+
+import { tsupBaseConfig } from "../shared/tsup-base.config";
 
 export default defineConfig([
 	{
-		entry: ['src/index.ts'],
-		format: ['esm'],
-		target: 'es2022',
-		// Bundle the whole public type surface into a single `dist/index.d.ts`.
-		dts: true,
-		sourcemap: true,
-		clean: true,
-		bundle: true,
-		splitting: false,
-		treeshake: true,
-		minify: false,
-		platform: 'browser',
-		keepNames: true,
-		external: ["tslib", "react", "react-dom", "react-router-dom"],
-		esbuildOptions(options) {
-			options.keepNames = true;
-			options.logLevel = 'error';
-		}
+		...tsupBaseConfig as unknown as Options,
+		external: [...tsupBaseConfig.external, "react", "react-dom", "react-router-dom"],
+		platform: 'browser'
 	}
 ]);
